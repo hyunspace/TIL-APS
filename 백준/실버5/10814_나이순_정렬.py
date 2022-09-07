cdb59ea58fb85a9247267405cbb5bfd4fcb834a8
hyunspace <at.hyunspace@gmail.com>
2022-09-07 23:58:50 +0900

    22-09-07 백준

---
+++ 백준/실버5/10814_나이순_정렬.py
@@ -0,0 +1,24 @@
+n = int(input())
+# ages = [[] for _ in range(201)]
+# index = []
+#
+# for _ in range(n):
+#     result = input()
+#     age, name = result.split()
+#     age = int(age)
+#     ages[age].append(result)
+#     if age not in index:
+#         index.append(age)
+#
+# index.sort()
+#
+# for i in index:
+#     if ages[i]:
+#         for j in range(len(ages[i])):
+#             print(ages[i][j])
+
+people = [input() for _ in range(n)]
+people.sort(key= lambda x : x[0:3])
+for person in p
+    eople:
+    print(person)
