d569622cc58d6771f3975bdb534415c2646af077
hyunspace <at.hyunspace@gmail.com>
2022-09-13 19:58:17 +0900

    22-09-13 이코테

--- 백준/실버2/18352_특정거리의도시찾기.py
+++ 백준/실버2/18352_특정거리의도시찾기.py
@@ -1,33 +1,43 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 n, m, k, x = map(int, input().split())
 cities = [[] for _ in range(n+1)]
 for _ in range(m):
     index, target = map(int, input().split())
     cities[index].append(target)
-# answer = [[0] for _ in range(n+1)]
+distance = [0] * (n+1)
 
 def bfs(start):
+    answer = []
     queue = deque()
     visited = [0 for _ in range(n+1)]
     queue.append(start)
-    visited[start] = x
+    visited[start] = 1
     while queue:
-        size = len(queue)
-        for _ in range(size):
-            now = queue.popleft()
-            for next in cities[now]:
-                # 아직 방문 안 한 곳이라면
-                if visited[next] == 0:
-                    visited[next] = visited[now] + 1
-                    queue.append(next)
-    flag = 1
-    for i in range(1, m+1):
-        if visited[i] == k+1:
-            print(i)
-            flag = 0
-    if flag:
+        now = queue.popleft()
+        for next in cities[now]:
+            # 아직 방문 안 한 곳이라면
+            if visited[next] == 0:
+                visited[next] = 1
+                queue.append(next)
+                distance[next] = distance[now] + 1
+                if distance[next] == k:
+                    answer.append(next)
+    if len(answer) > 0:
+        answer.sort()
+        for city in answer:
+            print(city)
+    else:
         print(-1)
+    # flag = 1
+    # for i in range(1, n+1):
+    #     if visited[i] == k+1:
+    #         print(i)
+    #         flag = 0
+    # if flag:
+    #     print(-1)
 
 bfs(x)
 
