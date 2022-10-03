407b8a50a5b69cfe252dafee42d60b741e1b9f92
hyunspace <at.hyunspace@gmail.com>
2022-10-04 03:59:27 +0900

    22-10-03 백준

---
+++ 백준/브론즈2/5618_공약수.py
@@ -0,0 +1,11 @@
+# n = int(input())
+# nums = []
+# nums.extend(map(int, input().split()))
+#
+# for i in range(1, min(nums)+1):
+#     cnt = 0
+#     for j in nums:
+#         if j % i == 0:
+#             cnt += 1
+#     if cnt == len(nums):
+#         print(i)
