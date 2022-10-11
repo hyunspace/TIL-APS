0002f225e6176b191b884e1f113b6f4479e008d3
hyunspace <at.hyunspace@gmail.com>
2022-10-12 00:34:44 +0900

    22-10-12 백준

--- 백준/실버2/1654_랜선자르기.py
+++ 백준/실버2/1654_랜선자르기.py
@@ -25,19 +25,26 @@
 # 221011 나무자르기 => 이진탐색!
 k, n = map(int, input().split())
 lines = list(int(input()) for _ in range(k))
-start = 0
+start = 1 # valueerror 발생해서 0에서 1로 바꿈!
 end = max(lines)
 answer = []
 
 while start <= end:
     center = (start + end) // 2
+    if center == 0: # zerodivision error 방지용
+        break
     nums = 0
+    # print(f'start: {start}, end: {end}')
+    # print(f'center: {center}')
     for line in lines:
         nums += line // center
+    # print(f'만든 랜선 개수: {nums}')
     if nums > n:
+        answer.append(center)
         start = center + 1
     elif nums < n:
         end = center - 1
     else:
-        break
-print(center)
+        answer.append(center)
+        start = center + 1
+print(max(answer))
